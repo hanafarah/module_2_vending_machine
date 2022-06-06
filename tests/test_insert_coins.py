b6a8cab2aa8dd1873fbbbef451ca1eaa59ab6cd1
Hana Farah'git config --global user.name Hana <farahhan@sheridancollege.ca>
2022-06-06 18:06:47 -0400

    commit 2

---
+++ tests/test_insert_coins.py
@@ -0,0 +1,16 @@
+"""
+Tests the insert_coin function
+"""
+import pytest
+from vending_machine import insert_coin
+
+
+def test_insert_five():
+    """
+    Given 5 to an empty list of coins, 5 should be
+    appended.
+    """
+    inserted_coins = []
+    insert_coin(5, inserted_coins)
+
+    assert inserted_coins == [5]
